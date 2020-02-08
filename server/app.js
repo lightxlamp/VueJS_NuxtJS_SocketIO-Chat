7ff313d499dc9400f487269a14b8ea477d390447
lightxlamp <sheveliukhin.s@gmail.com>
2020-02-08 21:49:28 +0600

    Commit from command line. Test for error msg 2

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 // https://dev.to/lawrence_chinedu/causes-of-heroku-h10-app-crashed-error-and-how-to-solve-them-3jnl
-// Causes of Heroku H10-App Crashed Error.
+// Causes of Heroku H10-App Crashed Error
 var express = require("express");
 var app = express();
 // app.set('port', process.env.PORT || 1789); // overridden by a similar statement in bin/www
